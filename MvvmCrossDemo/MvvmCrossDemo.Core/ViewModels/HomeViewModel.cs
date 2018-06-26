c43f66fc289e478f7787ca068d8ff670983cc4ec
Xiaodi Yan <xiaodi.yan@intergen.co.nz>
2018-06-26 17:37:43 +1200

    Created the dev branch.

--- MvvmCrossDemo/MvvmCrossDemo.Core/ViewModels/HomeViewModel.cs
+++ MvvmCrossDemo/MvvmCrossDemo.Core/ViewModels/HomeViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.ViewModels;
 
@@ -8,6 +9,22 @@ namespace MvvmCrossDemo.Core.ViewModels
 {
     public class HomeViewModel : MvxViewModel
     {
+        public HomeViewModel()
+        {
+        }
+
+        public override void Prepare()
+        {
+            // This is the first method to be called after construction
+        }
+
+        public override Task Initialize()
+        {
+            // Async initialization, YEY!
+
+            return base.Initialize();
+        }
+
         public IMvxCommand ResetTextCommand => new MvxCommand(ResetText);
         private void ResetText()
         {
