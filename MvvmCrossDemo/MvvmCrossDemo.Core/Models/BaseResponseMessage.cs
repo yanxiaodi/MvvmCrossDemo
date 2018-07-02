30cc7bf5720880d138ee199d1d2cf3ff93bc050a
Xiaodi Yan <xiaodi.yan@intergen.co.nz>
2018-07-02 13:57:23 +1200

    Added the models.

---
+++ MvvmCrossDemo/MvvmCrossDemo.Core/Models/BaseResponseMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmCrossDemo.Core.Models
+{
+    public class BaseResponseMessage
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class ResponseMessage<T> : BaseResponseMessage
+    {
+        public T Result { get; set; }
+    }
+}
