739daaaac73948b8c127b448a7460cce62c76f14
Xiaodi Yan <xiaodi.yan@intergen.co.nz>
2018-07-12 11:51:33 +1200

    Added the iOS project.

---
+++ MvvmCrossDemo/MvvmCrossDemo.iOS/AppDelegate.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using MvvmCross.Platforms.Ios.Core;
+using UIKit;
+
+namespace Blank
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the
+    // User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+    [Register("AppDelegate")]
+    public class AppDelegate : MvxApplicationDelegate<MvxIosSetup<MvvmCrossDemo.Core.App>, MvvmCrossDemo.Core.App>
+    {
+        
+    }
+}
+
+
