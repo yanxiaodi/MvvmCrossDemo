ad8e7a1eba7edbd3aacea6772f728425fa4d6ba1
Xiaodi Yan <xiaodi.yan@intergen.co.nz>
2018-07-12 17:02:42 +1200

    Updated the iOS project.

--- MvvmCrossDemo/MvvmCrossDemo.iOS/Views/FirstView.cs
+++ MvvmCrossDemo/MvvmCrossDemo.iOS/Views/FirstView.cs
@@ -1,15 +1,27 @@
 ﻿using Foundation;
 using System;
+using System.Drawing;
 using MvvmCross.Platforms.Ios.Views;
+using MvvmCrossDemo.Core.ViewModels;
 using UIKit;
 
 namespace MvvmCrossDemo.iOS.Views
 {
     [MvxFromStoryboard("Main")]
-    public partial class FirstView : UIViewController
+    public partial class FirstView : MvxViewController<FirstViewModel>
     {
         public FirstView (IntPtr handle) : base (handle)
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var label = new UILabel(new Rectangle(10, 0, 300, 40))
+            {
+                Text = "Your Name"
+            };
+            Add(label);
         }
     }
 }
