25f282f93ce866c1b8eede4373c836eb67eb58b0
Xiaodi Yan <yan_xiaodi@hotmail.com>
2018-10-22 13:05:35 +1300

    Added the Xamarin.Forms projects.

---
+++ MvvmCrossDemo/MvvmCrossDemo.Forms.UI/Views/FirstView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.Forms.Views;
+using MvvmCrossDemo.Core.ViewModels;
+
+namespace MvvmCrossDemo.Forms.UI.Views
+{
+    public partial class FirstView : MvxContentPage<FirstViewModel>
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
