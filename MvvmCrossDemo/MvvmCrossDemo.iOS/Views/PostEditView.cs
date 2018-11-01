22e5db07c34c4704c854cf4cb01619ef7dbe7243
Xiaodi Yan <yan_xiaodi@hotmail.com>
2018-11-01 13:53:39 +1300

    Update the PostEditView for iOS.

---
+++ MvvmCrossDemo/MvvmCrossDemo.iOS/Views/PostEditView.cs
@@ -0,0 +1,28 @@
+﻿using Foundation;
+using System;
+using MvvmCross.Platforms.Ios.Views;
+using MvvmCrossDemo.Core.ViewModels;
+using UIKit;
+using MvvmCross.Binding.BindingContext;
+
+namespace MvvmCrossDemo.iOS.Views
+{
+    [MvxFromStoryboard(nameof(PostEditView))]
+    public partial class PostEditView : MvxViewController<PostEditViewModel>
+    {
+        public PostEditView (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var set = this.CreateBindingSet<PostEditView, PostEditViewModel>();
+            set.Bind(txtTitle).To(vm => vm.Post.Title).TwoWay();
+            set.Bind(txtBody).To(vm => vm.Post.Body).TwoWay();
+            set.Bind(btnCancel).To(vm => vm.CancelAsyncCommand);
+            set.Bind(btnOk).To(vm => vm.EditPostAsyncCommand);
+            set.Apply();
+        }
+    }
+}
--- MvvmCrossDemo/MvvmCrossDemo.iOS/Views/PostListView.cs
+++ MvvmCrossDemo/MvvmCrossDemo.iOS/Views/PostListView.cs
@@ -23,7 +23,7 @@ namespace MvvmCrossDemo.iOS.Views
         public override void ViewDidLoad()
         {
             base.ViewDidLoad();
-            //_source = new MvxStandardTableViewSource(TableView, UITableViewCellStyle.Subtitle, new NSString("PostItemViewCell"), "TitleText Post.Title; DetailText Post.Body");
+            //var _source = new MvxStandardTableViewSource(TableView, UITableViewCellStyle.Subtitle, new NSString("PostItemViewCell"), "TitleText Post.Title; DetailText Post.Body");
             _source = new PostListTableSource(TableView);
             TableView.Source = _source;
 
@@ -49,10 +49,11 @@ namespace MvvmCrossDemo.iOS.Views
         protected override UITableViewCell GetOrCreateCellFor(UITableView tableView, NSIndexPath indexPath, object item)
         {
             //var cell = (UITableViewCell) TableView.DequeueReusableCell(PostCellIdentifier, indexPath) ?? new UITableViewCell(UITableViewCellStyle.Subtitle, PostCellIdentifier);
-            // You must set the identifier of the table cell in the designer. Otherwise, you must register the table cell first in the code.
+            // You must set the identifier of the table cell in the designer. Otherwise, you must register the table cell first in the constructor.
             var cell = TableView.DequeueReusableCell(PostCellIdentifier, indexPath);
-            cell.TextLabel.Text = ((WrapperPostViewModel) item).Post.Title;
-            cell.DetailTextLabel.Text = ((WrapperPostViewModel) item).Post.Body;
+            cell.TextLabel.Text = ((WrapperPostViewModel)item).Post.Title;
+            cell.DetailTextLabel.Text = ((WrapperPostViewModel)item).Post.Body;
+            cell.Accessory = UITableViewCellAccessory.DetailDisclosureButton;
             return cell;
         }
 
@@ -61,7 +62,18 @@ namespace MvvmCrossDemo.iOS.Views
             base.RowSelected(tableView, indexPath);
             var item = this.SelectedItem;
             //System.Diagnostics.Debug.WriteLine((item as WrapperPostViewModel)?.Post.Title);
-            (item as WrapperPostViewModel)?.ShowPostDetailAsyncCommand.Execute();
+            ((WrapperPostViewModel)item)?.ShowPostDetailAsyncCommand
+                .Execute((WrapperPostViewModel)item);
+        }
+
+        public override void AccessoryButtonTapped(UITableView tableView, NSIndexPath indexPath)
+        {
+            base.AccessoryButtonTapped(tableView, indexPath);
+            var item = this.ItemsSource.Cast<WrapperPostViewModel>().ToList()[indexPath.Row];
+            //System.Diagnostics.Debug.WriteLine((item as WrapperPostViewModel)?.Post.Title);
+            item?.EditPostAsyncCommand.Execute(item);
+            //var controller = UIAlertController.Create((item as WrapperPostViewModel)?.Post.Title,
+            //    (item as WrapperPostViewModel)?.Post.Body, UIAlertControllerStyle.Alert);
         }
     }
 
