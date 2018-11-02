2eb64fcce912e1dad081f3e9b7f089522f3802f8
Xiaodi Yan <yan_xiaodi@hotmail.com>
2018-11-02 17:47:10 +1300

    Update the data-binding of TableCell.

--- MvvmCrossDemo/MvvmCrossDemo.iOS/Views/PostListView.cs
+++ MvvmCrossDemo/MvvmCrossDemo.iOS/Views/PostListView.cs
@@ -9,6 +9,7 @@ using MvvmCrossDemo.Core.Models;
 using MvvmCrossDemo.Core.ViewModels;
 using UIKit;
 using System.Linq;
+using CoreGraphics;
 
 namespace MvvmCrossDemo.iOS.Views
 {
@@ -39,16 +40,22 @@ namespace MvvmCrossDemo.iOS.Views
     public class PostListTableSource : MvxTableViewSource
     {
         private static readonly NSString PostCellIdentifier = new NSString("PostCell");
+        //public static readonly UINib Nib = UINib.FromName("PostCell", NSBundle.MainBundle);
 
         public PostListTableSource(UITableView tableView) : base(tableView)
         {
             //tableView.RegisterNibForCellReuse(UINib.FromName("PostCell", NSBundle.MainBundle), PostCellIdentifier);
+            tableView.RegisterClassForCellReuse(typeof(PostListTableCell), PostCellIdentifier);
         }
 
 
         protected override UITableViewCell GetOrCreateCellFor(UITableView tableView, NSIndexPath indexPath, object item)
         {
             //var cell = (UITableViewCell) TableView.DequeueReusableCell(PostCellIdentifier, indexPath) ?? new UITableViewCell(UITableViewCellStyle.Subtitle, PostCellIdentifier);
+            // If you need to difine a customized table cell, use this method:
+            // var cell = (PostListTableCell)TableView.DequeueReusableCell(PostCellIdentifier, indexPath);
+            // cell.UpdateData((WrapperPostViewModel)item);
+
             // You must set the identifier of the table cell in the designer. Otherwise, you must register the table cell first in the constructor.
             var cell = TableView.DequeueReusableCell(PostCellIdentifier, indexPath);
             cell.TextLabel.Text = ((WrapperPostViewModel)item).Post.Title;
@@ -72,9 +79,64 @@ namespace MvvmCrossDemo.iOS.Views
             var item = this.ItemsSource.Cast<WrapperPostViewModel>().ToList()[indexPath.Row];
             //System.Diagnostics.Debug.WriteLine((item as WrapperPostViewModel)?.Post.Title);
             item?.EditPostAsyncCommand.Execute(item);
-            //var controller = UIAlertController.Create((item as WrapperPostViewModel)?.Post.Title,
-            //    (item as WrapperPostViewModel)?.Post.Body, UIAlertControllerStyle.Alert);
         }
+    }
+
+    [Register("PostCell")]
+    public sealed class PostListTableCell : MvxTableViewCell
+    {
+        private UILabel lablePostTitle;
+        private UILabel lablePostBody;
+
+        public PostListTableCell(IntPtr handle) : base(handle)
+        {
+            CreateLayout();
+            InitializeBindings();
+        }
+
+        public override void LayoutSubviews()
+        {
+            base.LayoutSubviews();
+            var width = ContentView.Frame.Width;
+            lablePostTitle.Frame = new CGRect(20, 7, 100, 30);
+        }
+
+        //This method is one-way.
+        //public void UpdateData(WrapperPostViewModel post)
+        //{
+        //    lablePostTitle.Text = post.Post.Title;
+        //}
+
+        private void CreateLayout()
+        {
+            //const int offsetStart = 10;
+            Accessory = UITableViewCellAccessory.DisclosureIndicator;
+            //lblTest = new UILabel(new RectangleF(offsetStart, 0, 75, 40));
+            //lblTest = new UILabel(new RectangleF(UIScreen.MainScreen.Bounds.Right - 85, 0, 55, 40));
+            //lblTest.TextAlignment = UITextAlignment.Right;
+            //lblTest = new UILabel(new RectangleF(jobId.Frame.Right, 0, UIScreen.MainScreen.Bounds.Width - jobId.Frame.Width - hours.Frame.Width - (3 * offsetStart), 40));
+            //lblTest.AdjustsFontSizeToFitWidth = true;
+            //lblTest.Lines = 0;
+            //lblTest.Font = jobName.Font.WithSize(10);
+            //ContentView.AddSubviews(jobId, jobName, hours);
+            lablePostTitle = new UILabel();
+            lablePostBody = new UILabel();
+            Accessory = UITableViewCellAccessory.DisclosureIndicator;
+            ContentView.AddSubviews(lablePostTitle, lablePostBody);
+        }
+
+        private void InitializeBindings()
+        {
+            this.DelayBind(() =>
+            {
+                var set = this.CreateBindingSet<PostListTableCell, WrapperPostViewModel>();
+                set.Bind(lablePostTitle).To(vm => vm.Post.Title).TwoWay();
+                set.Bind(lablePostBody).To(vm => vm.Post.Body).TwoWay();
+                set.Apply();
+            });
+        }
+
+        
     }
 
 
