b8025b78dfe60f0fc12d425cd96e234852d9968c
Xiaodi Yan <yan_xiaodi@hotmail.com>
2019-01-08 14:58:33 +1300

    Adjust the UI.

--- MvxFormsMasterDetailDemo/src/MvxFormsMasterDetailDemo.Core/ViewModels/MenuViewModel.cs
+++ MvxFormsMasterDetailDemo/src/MvxFormsMasterDetailDemo.Core/ViewModels/MenuViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Threading.Tasks;
 using MvvmCross.Commands;
 using MvvmCross.Navigation;
@@ -33,20 +34,57 @@ namespace MvxFormsMasterDetailDemo.Core.ViewModels
         #endregion
 
 
+        #region ShowDetailPageAsyncCommand obsoleted;
+        //private IMvxAsyncCommand<string> _showDetailPageAsyncCommand;
+        //public IMvxAsyncCommand<string> ShowDetailPageAsyncCommand
+        //{
+        //    get
+        //    {
+        //        _showDetailPageAsyncCommand = _showDetailPageAsyncCommand ?? new MvxAsyncCommand<string>(ShowDetailPageAsync);
+        //        return _showDetailPageAsyncCommand;
+        //    }
+        //}
+        //private async Task ShowDetailPageAsync(string param)
+        //{
+        //    // Implement your logic here.
+        //    switch (param)
+        //    {
+        //        case "Contacts":
+        //            await _navigationService.Navigate<ContactsViewModel>();
+        //            break;
+        //        case "Todo":
+        //            await _navigationService.Navigate<TodoViewModel>();
+        //            break;
+        //        default:
+        //            break;
+        //    }
+        //    if (Application.Current.MainPage is MasterDetailPage masterDetailPage)
+        //    {
+        //        masterDetailPage.IsPresented = false;
+        //    }
+        //    else if (Application.Current.MainPage is NavigationPage navigationPage
+        //             && navigationPage.CurrentPage is MasterDetailPage nestedMasterDetail)
+        //    {
+        //        nestedMasterDetail.IsPresented = false;
+        //    }
+        //}
+        #endregion
+
+
         #region ShowDetailPageAsyncCommand;
-        private IMvxAsyncCommand<string> _showDetailPageAsyncCommand;
-        public IMvxAsyncCommand<string> ShowDetailPageAsyncCommand
+        private IMvxAsyncCommand _showDetailPageAsyncCommand;
+        public IMvxAsyncCommand ShowDetailPageAsyncCommand
         {
             get
             {
-                _showDetailPageAsyncCommand = _showDetailPageAsyncCommand ?? new MvxAsyncCommand<string>(ShowDetailPageAsync);
+                _showDetailPageAsyncCommand = _showDetailPageAsyncCommand ?? new MvxAsyncCommand(ShowDetailPageAsync);
                 return _showDetailPageAsyncCommand;
             }
         }
-        private async Task ShowDetailPageAsync(string param)
+        private async Task ShowDetailPageAsync()
         {
             // Implement your logic here.
-            switch (param)
+            switch (SelectedMenuItem)
             {
                 case "Contacts":
                     await _navigationService.Navigate<ContactsViewModel>();
@@ -66,6 +104,16 @@ namespace MvxFormsMasterDetailDemo.Core.ViewModels
             {
                 nestedMasterDetail.IsPresented = false;
             }
+        }
+        #endregion
+
+
+        #region SelectedMenuItem;
+        private string _selectedMenuItem;
+        public string SelectedMenuItem
+        {
+            get => _selectedMenuItem;
+            set => SetProperty(ref _selectedMenuItem, value);
         }
         #endregion
     }
